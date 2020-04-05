eff821373030434772844ab93ee6604bb447d09d
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-06 01:30:14 +0800

    add example

---
+++ Polymorphism/ExampleOverloading1.java
@@ -0,0 +1,38 @@
+public class ExampleOverloading1 {
+
+    public int addNumbers(int x, int y) {
+        return x + y;
+    }
+
+    public double addNumbers(double x, int y) {
+        return x + y;
+    }
+
+    public int addNumbers(int x, int y, int z) {
+        return x + y + z;
+    }
+
+
+    public static void main(String[] args) {
+
+        int total1, total2 = 0;
+        double total3 = 0.0;
+
+        ExampleOverloading1 exampleOverloading1 = new ExampleOverloading1();
+        total1 = exampleOverloading1.addNumbers(5, 9);
+        System.out.println(total1);
+
+        total2 = exampleOverloading1.addNumbers(5, 9, 20);
+        System.out.println(total2);
+
+        total3 = exampleOverloading1.addNumbers(5.0, 9);
+        System.out.println(total3);
+    }
+}
+
+
+/***
+ * 14
+ * 34
+ * 14.0
+ */
