258b446226a51c5d6957d36c93fa2f7dda1f5221
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-06 01:32:26 +0800

    add example person

---
+++ Polymorphism/ExampleOverriding2.java
@@ -0,0 +1,28 @@
+//Example with super.printColor();
+
+class SuperCar {
+
+    public void printColor() {
+        System.out.println("SuperCar: Red");
+    }
+}
+
+class SuperToyota extends SuperCar {
+
+    @Override
+    public void printColor() {
+        super.printColor();   // see here
+        System.out.println("SuperToyota: Blue");
+    }
+
+    public static void main(String[] args) {
+        SuperToyota superToyota = new SuperToyota();
+        superToyota.printColor();
+    }
+}
+
+
+/***
+ SuperCar: Red
+ SuperToyota: Blue
+ */
