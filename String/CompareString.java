4bf1425485743ae5ea83682dc16762adddec0fa9
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-06 01:18:20 +0800

    Create CompareString.java

---
+++ String/CompareString.java
@@ -0,0 +1,15 @@
+public class CompareString1 {
+    public static void main(String[] args) {
+        String x = "Java";
+        String y = "Java";
+        if (x == y) {
+            System.out.println("x equal to y");
+        } else {
+            System.out.println("x not equal to y");
+        }
+    }
+}
+
+/***
+ * x equal to y
+ */
