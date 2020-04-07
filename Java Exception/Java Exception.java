aad7b92e0e176f64e26e8ef8c68a79fb55570fff
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-07 18:06:26 +0800

    add Java Exception

---
+++ Java Exception/Java Exception.java
@@ -0,0 +1,29 @@
+
+package javaexception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class JavaException {
+
+  
+    public static void main(String[] args) {
+      
+        System.out.println("Start");
+        Scanner scanner = new Scanner(System.in);
+        try{
+        int result = scanner.nextInt();
+        System.out.println(result);
+            }
+        catch(InputMismatchException e){
+        System.out.println("InputMismatchException");
+           }
+        finally{
+        System.out.println("This is finaly message");  
+        
+        
+    }
+    System.out.println("Stop");
+}
+}
