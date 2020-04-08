f3318382b267426eb8f14dc36b1d295411ca3a80
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-09 03:00:37 +0800

    add honda

--- Abstract/Honda.java
+++ Abstract/Honda.java
@@ -1,27 +1,27 @@
-public class Honda extends Car {
+package car;
 
-    public void displayYear() {
+public abstract class Honda extends Car{
+  
+      @Override
+      public void displayYear() {
         System.out.println("Honda: displayYear");
     }
 
+      @Override
     public void displayModel() {
         System.out.println("Honda: displayModel");
     }
 
+      @Override
     public void displayPrice() {
         System.out.println("Honda: displayPrice");
     }
 
     public static void main(String[] args) {
-        Honda honda = new Honda();
+        Honda honda = new Honda() {};
         honda.displayYear();
         honda.displayModel();
         honda.displayPrice();
     }
 }
 
-/***
- * Honda: displayYear
- * Honda: displayModel
- * Honda: displayPrice
- */
