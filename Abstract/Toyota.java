f650fccfd40845002b32beb9e8ebbbfae4452c46
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-09 02:59:12 +0800

    add example

--- Abstract/Toyota.java
+++ Abstract/Toyota.java
@@ -1,27 +1,25 @@
-public class Toyota extends Car {
-
-    public void displayYear() {
+public abstract class Toyota extends Car{
+  
+      @Override
+      public void displayYear() {
         System.out.println("Toyota: displayYear");
     }
 
+      @Override
     public void displayModel() {
         System.out.println("Toyota: displayModel");
     }
 
+      @Override
     public void displayPrice() {
         System.out.println("Toyota: displayPrice");
     }
 
     public static void main(String[] args) {
-        Toyota toyota = new Toyota();
+        Toyota toyota = new Toyota() {};
         toyota.displayYear();
         toyota.displayModel();
         toyota.displayPrice();
     }
 }
 
-/***
- * Toyota: displayYear
- * Toyota: displayModel
- * Toyota: displayPrice
- */
