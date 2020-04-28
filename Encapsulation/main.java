01fe9efaabd0ccd8977d5a3267d19f2a205b8c00
fatin shahira <55240830+fatinshahira@users.noreply.github.com>
2020-04-28 17:45:16 +0800

    Create main.java

---
+++ Encapsulation/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        Student s = new Student();
+      
+        s.setName("fatin");
+        s.setAge(21);
+       
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+         
+    }
+    
+}
